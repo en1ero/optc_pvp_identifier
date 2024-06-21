2cbd0c2ff73adc651df2ad59f8d725d8caefc227
en1ero <marc@hartung-web.com>
2024-06-21 20:57:38 +0200

    corrections

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ from utils.stats_utils import*
 def main(thumbnail_path, screenshot_path):
     # Make file list and perceptual hashes from thumbnail directory
     file_list = make_file_list(thumbnail_path, '.png')
+    file_list.append('images/null.png')
+    
     unique_file_list = select_unique_files(file_list)
     id_path_dict = make_id_path_dict(file_list)
     target_hashes = create_perceptual_hashes(unique_file_list) 
 
     # Make file list from screenshots directory
     screenshot_list = make_file_list(screenshot_path, '.PNG')
-
+    screenshot_list = sorted(screenshot_list)
+    
     # Get matches from screenshots and perceptual hashes
     matches = getMatchesFromScreenshots(screenshot_list, target_hashes)
 
@@ -27,7 +30,7 @@ def main(thumbnail_path, screenshot_path):
     buildCollage(matches)
     
     # Counted Units and Combinations
-    build_ranked_collage(teams_list, id_path_dict, n_rows=25, n_max_units=5)
+    build_ranked_collage(teams_list, id_path_dict, rows=25, n_max_units=5)
 
 
 
